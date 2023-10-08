f06d296f91fad99e5747d6f8cfd75d1a4fd26185
IVAN TORRES MARCOS <ivatormar2@alu.edu.gva.es>
2023-10-08 13:22:07 +0200

    Arreglar div error

--- applyforIvanTorres.php
+++ applyforIvanTorres.php
@@ -1,5 +1,6 @@
 <?php
-require_once(__DIR__ . '/INC/functions.php');
+
+require_once(__DIR__ . '/INC/functions.inc.php');
 /**
 Crear una aplicación web que contenga un script PHP applyforTunombre.php con formulario POST para enviar los datos 
 de un candidato para una oferta de trabajo (usuario, nombre, apellidos, DNI, dirección, mail, teléfono, fecha de nacimiento).
@@ -39,11 +40,11 @@ Añade todos los archivos de htdocs a una carpeta con tu nombre y comprime esa c
 
 $userExpr = '/^[a-z0-9_]+$/'; //User: Debe contener solo letras minúsculas, números y guiones bajos.
 $nameandSurnamesExpr = '/^[A-z\s]+$/'; //Nombre y apellidos: Debe contener solo letras mayúsculas y minúsculas, y puede incluir espacios.
-$dniExpr = '/^\d{8}[A-HJ-NP-TV-Z]$/';
-$directionExpr = '/^[a-zA-Z0-9\s,.-\/]+$/';
-$emailExpr = '/^[\w.-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}$/';
-$mobileNumberExpr = '/^[\d\s()-]+$/';
-$birthDateExpr = '/^\d{2}-\d{2}-\d{4}$/';
+$dniExpr = '/^\d{8}[A-HJ-NP-TV-Z]$/'; //DNI: Debe seguir el patrón de 8 dígitos seguido de una letra en mayúscula (puede ser 'X', 'Y' o 'Z' para casos especiales).
+$directionExpr = '/^[a-zA-Z0-9\s,.-\/]+$/'; //Dirección: Puede contener letras mayúsculas y minúsculas, números, espacios y los siguientes caracteres especiales: coma, punto, guion, número de casa y barra diagonal.
+$emailExpr = '/^[\w.-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}$/'; //Email: Debe seguir el patrón de una o más letras, números, guiones, puntos o guiones bajos, seguidos de un símbolo de arroba (@), seguido de un dominio válido (p. ej., gmail.com).
+$mobileNumberExpr = '/^[\d\s()-]+$/'; //Teléfono: Puede contener solo números, y puede incluir paréntesis, guiones y espacios en formatos comunes de números de teléfono.
+$birthDateExpr = '/^\d{2}-\d{2}-\d{4}$/'; // Fecha de nacimiento: Puede contener solo números, y puede incluir paréntesis, guiones y espacios en formatos comunes de números de teléfono.
 
 $errorMessages = []; //Creamos un array para almacenar todos los errores que tengamos
 $requiredMessages = []; //Creamos un array para almacenar todos los requires que tengamos
@@ -51,11 +52,11 @@ $requiredMessages = []; //Creamos un array para almacenar todos los requires que
 
 
 if (isset($_POST['name'])) {
-    if (!isset($_POST['code']) || empty($_POST['code'])) { //Ponemos el empty para validar que no es un string vacío
+    if (!isset($_POST['user']) || empty($_POST['user'])) { //Ponemos el empty para validar que no es un string vacío
         $requiredMessages[] = 'El campo CODE es obligatorio.<br>';
     } else {
-        $_POST['code'] = trim($_POST['code']);
-        if (!preg_match($codeExpr, $_POST['code'])) {
+        $_POST['user'] = trim($_POST['user']);
+        if (!preg_match($userExpr, $_POST['user'])) {
             $errorMessages[] = 'El código debe tener una letra seguida de un guión seguido de 5 dígitos.';
         }
     }
@@ -64,58 +65,76 @@ if (isset($_POST['name'])) {
         $requiredMessages[] = 'El campo NAME es obligatorio.<br>';
     } else {
         $_POST['name'] = trim($_POST['name']); //Eliminamos los espacios
-        if (!preg_match($nameExpr, $_POST['name'])) {
+        if (!preg_match($nameandSurnamesExpr, $_POST['name'])) {
 
-            $errorMessages[] = 'El nombre debe contener solo letras (mínimo 3 y máximo 20).';
+            $errorMessages[] = 'Nombre y apellidos: Debe contener solo letras mayúsculas y minúsculas, y puede incluir espacios.';
         }
     }
-
-
-
-    if (!isset($_POST['price']) || empty($_POST['price'])) {
-        $requiredMessages[] = 'El campo PRICE es obligatorio.<br>';
+    if (!isset($_POST['surname']) || empty($_POST['surname'])) {
+        $requiredMessages[] = 'El campo surname es obligatorio.<br>';
     } else {
-        $_POST['price'] = trim($_POST['price']);
-        if (!preg_match($priceExpr, $_POST['price'])) {
-            $errorMessages[] = 'El precio debe ser un número decimal.';
+        $_POST['surname'] = trim($_POST['surname']); //Eliminamos los espacios
+        if (!preg_match($nameandSurnamesExpr, $_POST['surname'])) {
+
+            $errorMessages[] = 'Nombre y apellidos: Debe contener solo letras mayúsculas y minúsculas, y puede incluir espacios.';
         }
     }
 
 
-    if (!isset($_POST['description']) || empty($_POST['description'])) {
-        $requiredMessages[] = 'El campo DESCRIPTION es obligatorio.<br>';
+
+    if (!isset($_POST['dni']) || empty($_POST['dni'])) {
+        $requiredMessages[] = 'El campo dni es obligatorio.<br>';
     } else {
-        $_POST['description'] = trim($_POST['description']);
-        if (!preg_match($descriptionExpr, $_POST['description'])) {
-            $errorMessages[] = 'La descripción debe contener al menos 50 caracteres alfanuméricos.';
+        $_POST['dni'] = trim($_POST['dni']);
+        if (validarDNI($_POST['dni'])) {
+            $errorMessages[] = 'El DNI es VÁLIDO';
+        } else {
+            $errorMessages[] = 'El DNI es NO VÁLIDO';
+        }
+        if (!preg_match($dniExpr, $_POST['dni'])) {
+            $errorMessages[] = 'Debe seguir el patrón de 8 dígitos seguido de una letra en mayúscula para casos especiales.';
         }
     }
 
 
-    if (!isset($_POST['manufacturer']) || empty($_POST['manufacturer'])) {
-        $requiredMessages[] = 'El campo MANUFACTURER es obligatorio.<br>';
+    if (!isset($_POST['direction']) || empty($_POST['direction'])) {
+        $requiredMessages[] = 'El campo direction es obligatorio.<br>';
     } else {
-        $_POST['manufacturer'] = trim($_POST['manufacturer']);
-        if (!preg_match($manufacturerExpr, $_POST['manufacturer'])) {
-            $errorMessages[] = 'El fabricante debe contener entre 10 y 20 caracteres alfanuméricos.';
+        $_POST['direction'] = trim($_POST['direction']);
+        if (!preg_match($directionExpr, $_POST['direction'])) {
+            $errorMessages[] = 'Puede contener letras mayúsculas y minúsculas, números, espacios y los siguientes caracteres especiales: coma, punto, guion, número de casa y barra diagonal.';
         }
     }
 
-    if (!isset($_POST['acquisitionDate']) || empty($_POST['acquisitionDate'])) {
+
+    if (!isset($_POST['email']) || empty($_POST['email'])) {
+        $requiredMessages[] = 'El campo email es obligatorio.<br>';
+    } else {
+        $_POST['email'] = trim($_POST['email']);
+        if (!preg_match($emailExpr, $_POST['email'])) {
+            $errorMessages[] = 'Debe seguir el patrón de una o más letras, números, guiones, puntos o guiones bajos, seguidos de un símbolo de arroba (@), seguido de un dominio válido (p. ej., gmail.com).';
+        }
+    }
+
+    if (!isset($_POST['mobilephoneNumber']) || empty($_POST['mobilephoneNumber'])) {
+        $requiredMessages[] = 'El campo mobilephoneNumber es obligatorio.<br>';
+    } else {
+        $_POST['mobilephoneNumber'] = trim($_POST['mobilephoneNumber']);
+        if (!preg_match($mobilephoneNumberExpr, $_POST['mobilephoneNumber'])) {
+            $errorMessages[] = 'Debe seguir el patrón de una o más letras, números, guiones, puntos o guiones bajos, seguidos de un símbolo de arroba (@), seguido de un dominio válido (p. ej., gmail.com).';
+        }
+    }
+
+    if (!isset($_POST['birthDate']) || empty($_POST['birthDate'])) {
         $requiredMessages[] = 'El campo  ACQUISITION DATE es obligatorio.<br>';
     } else {
-        $_POST['acquisitionDate'] = trim($_POST['acquisitionDate']);
-        if (!preg_match($dateExpr, $_POST['acquisitionDate'])) {
+        $_POST['birthDate'] = trim($_POST['birthDate']);
+        if (!preg_match($birthDateExpr, $_POST['birthDate'])) {
             $errorMessages[] = 'La fecha de adquisición debe tener el formato dd/mm/yyyy.';
         }
     }
 }
-$dni = '44535252F';
-if (validarDNI($dni)) {
-    echo 'DNI APTO';
-} else {
-    echo 'DNI NO APTO';
-}
+
 
 
 
@@ -127,9 +146,7 @@ if (validarDNI($dni)) {
 <html lang="es">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Product Ivan Torres</title>
+    <?php require_once(__DIR__ . '/INC/header.inc.php'); ?>
 </head>
 
 <body>
@@ -145,9 +162,11 @@ if (validarDNI($dni)) {
     }
     echo '</div>';
 
-    echo '<div>'; //Mostramos los errores
-    foreach ($errorMessages as $message) {
-        echo $message . '<br>';
+    echo '<div class="divError>'; //Mostramos los errores //TODO ESTO HAY QUE ARREGLARLO
+    if (count($errorMessages) == 0) {
+        foreach ($errorMessages as $message) {
+            echo $message . '<br>';
+        }
     }
     echo '</div>';
 
@@ -160,7 +179,7 @@ if (validarDNI($dni)) {
         Dirección <input type="text" name="direction"><br>
         Email <input type="text" name="email"><br>
         Teléfono <input type="text" name="mobilephoneNumber"><br>
-        Fecha de nacimiento <input type="text" name="acquisitionDate"><br>
+        Fecha de nacimiento <input type="text" name="birthDate"><br>
         <input type="submit" value="Enviar">
     </form>
 </body>
