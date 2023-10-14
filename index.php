dd7bed02a741dc2040b77cdf60d1fe03831761e4
IVAN TORRES MARCOS <ivatormar2@alu.edu.gva.es>
2023-10-14 20:40:06 +0200

    Falta <a> login + register

--- index.php
+++ index.php
@@ -9,88 +9,132 @@
     <title>INDEXXX</title>
 </head>
 
-<body>
 
-    <body cz-shortcut-listen="true">
-        <!-- Navbar -->
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    <img src="/MEDIA-REVELS-LOGO/logo-meouwth.png" alt="logoNav">
-                </a>
-                <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <form class="me-3">
-                        <div class="form-white input-group" style="width: 250px;">
-                            <input type="search" class="form-control rounded" placeholder="BIG PILILAS" aria-label="Search" aria-describedby="search-addon">
-                        </div>
-                    </form>
-                </div>
-                <div class="button-container">
-                    <button type="button" id="btnRegister" class="btn btn-rounded" data-mdb-ripple-color="#ffffff" style="background-color:#fc92ad">REGISTER</button>
-                    <button type="button" class="btn btn-rounded" data-mdb-ripple-color="#ffffff" style="background-color:#fc92ad">LOGIN</button>
-                </div>
+
+<body cz-shortcut-listen="true">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="/MEDIA-REVELS-LOGO/logo-meouwth.png" alt="logoNav">
+            </a>
+            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <form class="me-3">
+                    <div class="form-white input-group" style="width: 250px;">
+                        <input type="search" class="form-control rounded" placeholder="BIG PILILAS" aria-label="Search" aria-describedby="search-addon">
+                    </div>
+                </form>
             </div>
-        </nav>
+            <div class="button-container">
+                <button type="button" id="btnRegister" class="btn btn-rounded" data-mdb-ripple-color="#ffffff" style="background-color:#fc92ad">REGISTER</button>
+                <button type="button" id="btnLogin" class="btn btn-rounded" data-mdb-ripple-color="#ffffff" style="background-color:#fc92ad">LOGIN</button>
+            </div>
+        </div>
+    </nav>
 
-        <section class="vh-100 bg-image" style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp');">
-            <div class="mask d-flex align-items-center h-100 gradient-custom-3">
-                <div class="container h-100">
-                    <div class="row d-flex justify-content-center align-items-center h-100">
-                        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-                            <div class="card" style="border-radius: 15px;">
-                                <div class="card-body p-5">
-                                    <h2 class="text-uppercase text-center mb-5">Create an account</h2>
+    <!-- Register form -->
+    <section id="seccionRegister" class="vh-100 bg-image " style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp'); display:none;">
+        <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+            <div class="container h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+                        <div class="card" style="border-radius: 15px;">
+                            <div class="card-body p-5">
+                                <h2 class="text-uppercase text-center mb-5">Create an account</h2>
 
-                                    <form>
+                                <form>
 
-                                        <div class="form-outline mb-4">
-                                            <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
-                                            <label class="form-label" for="form3Example1cg">Your Name</label>
-                                        </div>
+                                    <div class="form-outline mb-4">
+                                        <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
+                                        <label class="form-label" for="form3Example1cg">Your Name</label>
+                                    </div>
 
-                                        <div class="form-outline mb-4">
-                                            <input type="email" id="form3Example3cg" class="form-control form-control-lg" />
-                                            <label class="form-label" for="form3Example3cg">Your Email</label>
-                                        </div>
+                                    <div class="form-outline mb-4">
+                                        <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
+                                        <label class="form-label" for="form3Example1cg">Your User</label>
+                                    </div>
 
-                                        <div class="form-outline mb-4">
-                                            <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
-                                            <label class="form-label" for="form3Example4cg">Password</label>
-                                        </div>
+                                    <div class="form-outline mb-4">
+                                        <input type="email" id="form3Example3cg" class="form-control form-control-lg" />
+                                        <label class="form-label" for="form3Example3cg">Your Email</label>
+                                    </div>
 
-                                        <div class="form-outline mb-4">
-                                            <input type="password" id="form3Example4cdg" class="form-control form-control-lg" />
-                                            <label class="form-label" for="form3Example4cdg">Repeat your password</label>
-                                        </div>
+                                    <div class="form-outline mb-4">
+                                        <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
+                                        <label class="form-label" for="form3Example4cg">Password</label>
+                                    </div>
 
-                                        <div class="form-check d-flex justify-content-center mb-5">
-                                            <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3cg" />
-                                            <label class="form-check-label" for="form2Example3g">
-                                                I agree all statements in <a href="#!" class="text-body"><u>Terms of service</u></a>
-                                            </label>
-                                        </div>
+                                    <div class="form-outline mb-4">
+                                        <input type="password" id="form3Example4cdg" class="form-control form-control-lg" />
+                                        <label class="form-label" for="form3Example4cdg">Repeat your password</label>
+                                    </div>
 
-                                        <div class="d-flex justify-content-center">
-                                            <button type="button" class="btn  btn-block btn-lg gradient-custom-4 text-body">Register</button>
-                                        </div>
+                                    <div class="form-check d-flex justify-content-center mb-5">
+                                        <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3cg" />
+                                        <label class="form-check-label" for="form2Example3g">
+                                            I agree all statements in <a href="#!" class="text-body"><u>Terms of service</u></a>
+                                        </label>
+                                    </div>
 
-                                        <p class="text-center text-muted mt-5 mb-0">Have already an account? <a href="#!" class="fw-bold text-body"><u>Login here</u></a></p>
+                                    <div class="d-flex justify-content-center">
+                                        <button type="button" class="btn  btn-block btn-lg gradient-custom-4 text-body">Register</button>
+                                    </div>
 
-                                    </form>
+                                    <p class="text-center text-muted mt-5 mb-0">Have already an account? <a href="#!" class="fw-bold text-body" id="aLoginHere"><u>Login here</u></a></p>
 
-                                </div>
+                                </form>
+
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
 
-    </body>
+    <!-- Login form -->
+    <section id="seccionLogin" class="vh-100 bg-image " style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp'); display:none;">
+        <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+            <div class="container h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+                        <div class="card" style="border-radius: 15px;">
+                            <div class="card-body p-5">
+                                <h2 class="text-uppercase text-center mb-5">Login to you account</h2>
 
+                                <form>
+
+                                    <div class="form-outline mb-4">
+                                        <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
+                                        <label class="form-label" for="form3Example1cg">Your User</label>
+                                    </div>
+
+                                    <div class="form-outline mb-4">
+                                        <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
+                                        <label class="form-label" for="form3Example4cg">Password</label>
+                                    </div>
+
+                                    <div class="d-flex justify-content-center">
+                                        <button type="button" class="btn  btn-block btn-lg gradient-custom-4 text-body">Login</button>
+                                    </div>
+
+                                    <p class="text-center text-muted mt-5 mb-0">Don't have an account? <a href="#!" class="fw-bold text-body" id="aRegisterHere"><u>Register here</u></a></p>
+
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="main.js"></script>
 </body>
+
+
 
 </html>
--- main.js
+++ main.js
@@ -2,14 +2,39 @@
 
 // Get the "Register" button and the form
 let btnRegister = document.getElementById("btnRegister");
-let form = document.querySelector(".vh-100");
+let btnLogin = document.getElementById("btnLogin");
+
+let aLoginHere = document.getElementById("aLoginHere");
+let aRegisterHere = document.getElementById("aRegisterHere");
+
+let formRegister = document.getElementById("seccionRegister");
+let formLogin = document.getElementById("seccionLogin");
+
+btnLogin.addEventListener("click", () => {
+  console.log("LOGIN");
+  if (formLogin.style.display === "none") {
+    formLogin.style.display = "block";
+  } else {
+    formLogin.style.display = "none";
+  }
+});
+
+aLoginHere.addEventListener("click", () => {
+  console.log("Login dentro del register");
+});
+
+aRegisterHere.addEventListener("click", () => {
+  console.log("Register dentro del login");
+});
 
 // Add an event listener to the "Register" button
 btnRegister.addEventListener("click", () => {
+  console.log("REGISTER");
+
   // Toggle the visibility of the form
-  if (form.style.display === "none") {
-    form.style.display = "block";
+  if (formRegister.style.display === "none") {
+    formRegister.style.display = "block";
   } else {
-    form.style.display = "none";
+    formRegister.style.display = "none";
   }
 });
