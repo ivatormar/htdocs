8c003aa0bed08c345c40e7f41a254d9661359721
Ivan <ivatormar2@alu.edu.gva.es>
2023-10-19 20:33:29 +0200

    ACABADO al 100x100

--- watermark.php
+++ watermark.php
@@ -9,7 +9,7 @@ $image=imagecreatefromstring(file_get_contents($_GET['img']));
 $blueColor=imagecolorallocatealpha($image,0,191,255,75);
 $myName='IVAN TORRES';
 
-imagettftext($image, 35, 0, 11, 75, $blueColor, __DIR__.'/Bulletto-Killa.ttf', $myName);
+imagettftext($image, 35, 0, 11, 75, $blueColor, __DIR__.'/FONT/Bulletto-Killa.ttf', $myName);
 
 header('Content-type:image/png');
 imagepng($image);
