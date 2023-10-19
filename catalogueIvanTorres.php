3c5fcfa3e8a2c48621efb6677de590b69eb292f4
Ivan <ivatormar2@alu.edu.gva.es>
2023-10-19 20:32:01 +0200

    100% HECHO

--- catalogueIvanTorres.php
+++ catalogueIvanTorres.php
@@ -3,17 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="/CSS/style.css">
+    <title>Iván Torres Marcos</title>
 </head>
 <body>
+    <h1>Catálogo Online</h1>
 <?php
+/**
+ * @author Tu Nombre
+ * @version 1.2
+ * @description Un simple foreach en el que "llamamos" el script de generar la marca de agua cada vez que recorremos la imagen
+ *
+ */
+
     $dir = __DIR__."/IMG/";
     $files = glob($dir."*.png");
     
+
     foreach($files as $img) {
+        echo '<div class="imgDiv">';
         
             echo '<img src="watermark.php?img='.$img. '" alt="'.$img.'" />';
-        
+        echo '</div>';
     }
     ?>
 </body>
--- watermark.php
+++ watermark.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * @author Iván Torres Marcos
+ * @version 1.2
+ * @description Creación de la marca de agua
+ *
+ */
 $image=imagecreatefromstring(file_get_contents($_GET['img']));
 $blueColor=imagecolorallocatealpha($image,0,191,255,75);
 $myName='IVAN TORRES';
