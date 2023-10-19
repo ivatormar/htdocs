46ee48e3cc0dc7b80f6bdac46c23f88c596c728c
Ivan <ivatormar2@alu.edu.gva.es>
2023-10-19 20:54:26 +0200

    ACABADO

--- catalogueIvanTorres.php
+++ catalogueIvanTorres.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/CSS/style.css">
+    <link rel="shortcut icon" href="/descarga-removebg-preview_1.ico" type="image/x-icon">
     <title>Iván Torres Marcos</title>
 </head>
 <body>
