c5c9d469bb19751f2b446ebf0ba4850399149f38
Ivan <ivatormar2@alu.edu.gva.es>
2023-10-23 18:28:37 +0200

    Acabado 1000%

--- INC/User.inc.php
+++ INC/User.inc.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
  * @author Iván Torres Marcos
  * @version 1.1
- * @description Creación de la clase user con susfunciones
+ * @description Creación de la clase user con sus funciones
  *
  */
 
@@ -17,7 +18,7 @@ class User
     private $birthday;
     private $phone;
 
-    public function __construct($id, $email, $name, $surname1='', $surname2='', $birthday='01/05/1980', $phone='')
+    public function __construct($id, $email, $name, $surname1 = '', $surname2 = '', $birthday = '01/05/1980', $phone = '')
     {
         $this->id = $id;
         $this->email = $email;
@@ -28,34 +29,38 @@ class User
         $this->phone = $phone;
     }
 
-    public function __set($property,$value){
-        if(isset($this->$property))
-            $this->$property=$value;
-        
+    public function __set($property, $value)
+    {
+        if (isset($this->$property))
+            $this->$property = $value;
     }
 
 
-    public function __get($property){
-        if(isset($this->$property))
+    public function __get($property)
+    {
+        if (isset($this->$property))
             return $this->$property;
     }
-    
-    public function calculate_age() {
+
+    public function calculate_age()
+    {
         $birthday = new DateTime();
         $birthday->setTimestamp($this->birthday);
         $now = new DateTime();
         $age = $now->diff($birthday)->y;
         return $age;
     }
-    
-    function make_birthday(): int {
+
+    function make_birthday(): int
+    {
         $birthday = new DateTime();
         $birthday->setDate(mt_rand(1999, 2005), mt_rand(1, 12), mt_rand(1, 31));
         $birthday->setTime(0, 0, 0);
         return $birthday->getTimestamp();
     }
 
-    private function calculate_birthdate() {
+    private function calculate_birthdate()
+    {
         $birthday = new DateTime();
         $birthday->setTimestamp($this->birthday); // Use the user's stored birthday timestamp
         $day = $birthday->format('d');
@@ -76,20 +81,19 @@ class User
         ];
         $month = $monthNames[$monthNumber];
         $year = $birthday->format('Y');
-        
+
         return $day . ' de ' . $month . ' de ' . $year;
     }
-    
+
 
 
     public function __toString()
     {
         return '<article class="user">
-                <h1>'.$this->name.' '.$this->surname1.' '.$this->surname2.' ('.$this->id.')</h1>
+                <h1>' . $this->name . ' ' . $this->surname1 . ' ' . $this->surname2 . ' (' . $this->id . ')</h1>
                 <div>
-                <p>'.self::calculate_age($this->birthday).' - '.self::calculate_birthdate($this->birthday).'<br> Email: <a href="'.$this->email.'">'.$this->email.'</a><br> Teléfono:  '.$this->phone.'</p>
+                <p>' . self::calculate_age($this->birthday) . ' años - ' . self::calculate_birthdate($this->birthday) . '<br> Email: <a href="' . $this->email . '">' . $this->email . '</a><br> Teléfono:  ' . $this->phone . '</p>
                 </div> 
                 </article><br>';
     }
-    
 }
