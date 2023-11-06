3a0ea8dfb14a89eb57f00e709e327c9c65b26a42
Ivan <ivatormar2@alu.edu.gva.es>
2023-11-06 14:45:44 +0100

    Pequeño cambio

--- INC/login.inc.php
+++ INC/login.inc.php
@@ -62,7 +62,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/CSS/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>INDEXXX</title>
+    <title>Revels.</title>
 </head>
 
 <body cz-shortcut-listen="true">
