2a51193c31ee1fc903b780d2814637f0367c7f34
Ivan <ivatormar2@alu.edu.gva.es>
2023-11-29 20:17:17 +0100

    Mercachop

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,32 +1,61 @@
 <?php
-   /**
-    * @author Ivan Torres Marcos
-    * @version 1.1
-    * @description Cabecera de nuestro codigo para mostrar una cosa u otra en función del rol del usuario
-    *
-    */
-   
-   echo '<header>';
-   echo '<h1><a href="/index">MerchaShop</a></h1>';
-   
-   // Verificar si el usuario está logueado
-   if (isset($_SESSION['user'])) {
-       // Usuario logueado
-       echo 'Bienvenido, ' . $_SESSION['user']['user'];
-        // Verificar el rol del usuario
-        if (isset($_SESSION['user']['rol']) && $_SESSION['user']['rol'] === 'admin') {
-           echo '<br><a href="/users.php">Administrar usuarios</a>';
-       }
-       echo '<br><a href="/logout.php">Cerrar sesión</a>';
-   
-      
-   } else {
-       // Usuario no logueado
-       echo '<a href="/index">Principal</a><br>';
-       
-       // Añadir enlaces de registro e inicio de sesión
-       echo '<a href="/login.php">Iniciar sesión</a><br>';
-       echo '<a href="/signup.php">Registrarse</a>';
-   }
-   echo '</header>';
-?>
+
+/**
+ * @author Ivan Torres Marcos
+ * @version 1.1
+ * @description Cabecera de nuestro codigo para mostrar una cosa u otra en función del rol del usuario
+ *
+ */
+
+$preferredLanguage = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+
+$defaultLanguage = 'en';
+
+// Check if the preferred language is available
+if (strpos($preferredLanguage, 'es') !== false) {
+    $defaultLanguage = 'es'; // Set the default language to Spanish if available
+} elseif (strpos($preferredLanguage, 'val') !== false) {
+    $defaultLanguage = 'val'; // Set the default language to Valencian if available
+}
+
+$disableFlag = '';
+if ($defaultLanguage === 'es') {
+    $disableFlag = 'spainFlag';
+} elseif ($defaultLanguage === 'en') {
+    $disableFlag = 'englishFlag';
+} elseif ($defaultLanguage === 'val') {
+    $disableFlag = 'valencianFlag';
+}
+
+echo '<header>';
+echo '<h1><a href="/index">MerchaShop</a></h1>';
+
+echo '<div class="flags">';
+echo '<a href="/index?lang=es" class="spainFlag' . ($disableFlag === 'spainFlag' ? ' disabled' : '') . '"><img src="/img/bandera_españa.png" style="width:50px"></a>';
+echo '<a href="/index?lang=en" class="englishFlag' . ($disableFlag === 'englishFlag' ? ' disabled' : '') . '"><img src="/img/bandera_inglaterra.jpg" style="width:50px"></a>';
+echo '<a href="/index?lang=val" class="valencianFlag' . ($disableFlag === 'valencianFlag' ? ' disabled' : '') . '"><img src="/img/bandera_valencia.png" style="width:50px"></a>';
+echo '</div>';
+
+echo '</div>';
+
+// Verificar si el usuario está logueado
+if (isset($_SESSION['user'])) {
+    // Usuario logueado
+    echo 'Bienvenido, ' . $_SESSION['user']['user'];
+    // Verificar el rol del usuario
+    if (isset($_SESSION['user']['rol']) && $_SESSION['user']['rol'] === 'admin') {
+        echo '<br><a href="/users.php">Administrar usuarios</a>';
+    }
+    echo '<br><a href="/logout.php">Cerrar sesión</a>';
+} else {
+    // Usuario no logueado
+    echo '<a href="/index">Principal</a><br>';
+
+    // Añadir enlaces de registro e inicio de sesión
+    echo '<a href="/login.php">Iniciar sesión</a><br>';
+    echo '<a href="/signup.php">Registrarse</a>';
+}
+
+
+
+echo '</header>';
--- index.php
+++ index.php
@@ -1,120 +1,155 @@
 <?php
-   /**
-    * @author Alex Torres 
-    * @version 1.0
-    * @description Indice donde mostramos y añadimos la funcionalidad de los productos
-    *
-    */
-   
-   
-   session_start();
-   
-   
-   // Verificar si hay un mensaje de éxito en la sesión, esto es para cuando venimos del resetPassword.php
-   if (isset($_SESSION['success'])) {
-       echo '<div class="success-message">' . $_SESSION['success'] . '</div>';
-       // Eliminar el mensaje de éxito para que no se muestre en futuras visitas
-       unset($_SESSION['success']);
+
+/**
+ * @author Alex Torres 
+ * @version 1.0
+ * @description Indice donde mostramos y añadimos la funcionalidad de los productos
+ *
+ */
+
+
+session_start();
+if (isset($_GET['lang'])) {
+   // Validate and set the language based on the parameter
+   $allowedLanguages = ['es', 'val', 'en'];
+   $selectedLanguage = $_GET['lang'];
+
+   if (in_array($selectedLanguage, $allowedLanguages)) {
+      $_SESSION['language'] = $selectedLanguage;
+
+      // Set a cookie to remember the selected language
+      setcookie('preferred_language', $selectedLanguage, time() + (365 * 24 * 60 * 60), '/');
    }
-   
-   // Lógica para manejar la actualización del carrito
-   if (isset($_GET['add']) || isset($_GET['subtract']) || isset($_GET['remove'])) {
-       if (isset($_GET['add']) && $_GET['add'] != '') {
-           if (!isset($_SESSION['basket'][$_GET['add']]))
-               $_SESSION['basket'][$_GET['add']] = 1;
-           else
-               $_SESSION['basket'][$_GET['add']] += 1;
-       }
-       if (isset($_GET['subtract']) && $_GET['subtract'] != '' && isset($_SESSION['basket'][$_GET['subtract']])) {
-           $_SESSION['basket'][$_GET['subtract']] -= 1;
-           if ($_SESSION['basket'][$_GET['subtract']] <= 0)
-               unset($_SESSION['basket'][$_GET['subtract']]);
-       }
-       if (isset($_GET['remove']) && $_GET['remove'] != '' && isset($_SESSION['basket'][$_GET['remove']])) {
-           unset($_SESSION['basket'][$_GET['remove']]);
-       }
+} elseif (isset($_COOKIE['preferred_language'])) {
+   // Use the language stored in the cookie
+   $_SESSION['language'] = $_COOKIE['preferred_language'];
+}
+
+// Include the language file based on the selected language or use the default language
+$language = isset($_SESSION['language']) ? $_SESSION['language'] : 'en'; // Default to English if no language is set
+
+// Load the appropriate language file
+switch ($language) {
+   case 'es':
+      require_once(__DIR__ . '/includes/es_ES.lang.php');
+      break;
+   case 'en':
+      require_once(__DIR__ . '/includes/en_EN.inc.php');
+      break;
+   case 'val':
+      require_once(__DIR__ . '/includes/ca_CA.inc.php');
+      break;
+   default:
+      // Fallback to English if the language is not recognized
+      require_once(__DIR__ . '/includes/en_EN.inc.php');
+}
+
+// Verificar si hay un mensaje de éxito en la sesión, esto es para cuando venimos del resetPassword.php
+if (isset($_SESSION['success'])) {
+   echo '<div class="success-message">' . $_SESSION['success'] . '</div>';
+   // Eliminar el mensaje de éxito para que no se muestre en futuras visitas
+   unset($_SESSION['success']);
+}
+
+// Lógica para manejar la actualización del carrito
+if (isset($_GET['add']) || isset($_GET['subtract']) || isset($_GET['remove'])) {
+   if (isset($_GET['add']) && $_GET['add'] != '') {
+      if (!isset($_SESSION['basket'][$_GET['add']]))
+         $_SESSION['basket'][$_GET['add']] = 1;
+      else
+         $_SESSION['basket'][$_GET['add']] += 1;
    }
+   if (isset($_GET['subtract']) && $_GET['subtract'] != '' && isset($_SESSION['basket'][$_GET['subtract']])) {
+      $_SESSION['basket'][$_GET['subtract']] -= 1;
+      if ($_SESSION['basket'][$_GET['subtract']] <= 0)
+         unset($_SESSION['basket'][$_GET['subtract']]);
+   }
+   if (isset($_GET['remove']) && $_GET['remove'] != '' && isset($_SESSION['basket'][$_GET['remove']])) {
+      unset($_SESSION['basket'][$_GET['remove']]);
+   }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
-   <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>MerchaShop</title>
-      <link rel="stylesheet" href="/css/style.css">
-   </head>
-   <body>
-      <?php
-         require_once(__DIR__ . '/includes/header.inc.php');
-         if (!isset($_SESSION['user'])) : ?>
+
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>MerchaShop</title>
+   <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+   <?php
+   require_once(__DIR__ . '/includes/header.inc.php');
+   if (!isset($_SESSION['user'])) : ?>
       <!-- Mostrar formulario de registro para usuarios no logueados -->
       <section class="registro">
-         <h2>Registro</h2>
-         <form action="/signup.php" method="post">
-            <label for="username">Usuario:</label>
-            <input type="text" id="username" name="username" required>
-            <label for="email">Correo electrónico:</label>
-            <input type="email" id="email" name="email" required>
-            <label for="password">Contraseña:</label>
-            <input type="password" id="password" name="password" required>
-            <button type="submit">Registrarse</button>
-         </form>
-         <p>¿Ya tienes una cuenta? <a href="/login.php">Inicia sesión aquí</a>.</p>
+         <!-- Replace static content with translations -->
+         <h2><?php echo $lang['registro']; ?></h2>
+         <label for="username"><?php echo $lang['usuario']; ?>:</label>
+         <label for="email"><?php echo $lang['correo']; ?>:</label>
+         <label for="password"><?php echo $lang['contraseña']; ?>:</label>
+         <button type="submit"><?php echo $lang['registrarse']; ?></button>
+         <p><?php echo $lang['cuenta']; ?> <a href="/login.php"><?php echo $lang['iniciar_sesion']; ?></a>.</p>
+
       </section>
       <!-- Enlace a sales.php -->
       <section class="sales-link">
          <a href="/sales.php">
-         <img id="sales_image" src="/img/sales_image.png" alt="Ofertas">
+            <img id="sales_image" src="/img/sales_image.png" alt="Ofertas">
          </a>
       </section>
-      <?php else : ?>
+   <?php else : ?>
       <!-- Mostrar lista de productos para usuarios logueados -->
       <div id="carrito">
          <?php
-            if (!isset($_SESSION['basket']))
-                $products = 0;
-            else
-                $products = count($_SESSION['basket']);
-            echo $products;
-            echo ' producto';
-            if ($products > 1)
-                echo 's';
-            ?>
+         if (!isset($_SESSION['basket']))
+            $products = 0;
+         else
+            $products = count($_SESSION['basket']);
+         echo $products;
+         echo ' producto';
+         if ($products > 1)
+            echo 's';
+         ?>
          en el carrito.
          <a href="/basket" class="boton">Ver carrito</a>
       </div>
       <section class="productos">
          <?php
-            include_once(__DIR__ . '/includes/dbconnection.inc.php');
-            $connection = getDBConnection();
-            $products = $connection->query('SELECT * FROM products;', PDO::FETCH_OBJ);
-            
-            ?>
+         include_once(__DIR__ . '/includes/dbconnection.inc.php');
+         $connection = getDBConnection();
+         $products = $connection->query('SELECT * FROM products;', PDO::FETCH_OBJ);
+
+         ?>
          <section class="sales-link"><br>
             <a href="/sales.php">
-            <img id="sales_image" src="/img/sales_image.png" alt="Ofertas">
+               <img id="sales_image" src="/img/sales_image.png" alt="Ofertas">
             </a>
          </section>
          <?php
-            foreach ($products as $product) {
-                echo '<article class="producto">';
-                echo '<h2>' . $product->name . '</h2>';
-                echo '<span>(' . $product->category . ')</span>';
-                echo '<img src="/img/products/' . $product->image . '" alt="' . $product->name . '" class="imgProducto"><br>';
-                echo '<span>' . $product->price . ' €</span><br>';
-                echo '<span class="botonesCarrito">';
-                echo '<a href="/add/' . $product->id . '" class="productos"><img src="/img/mas.png" alt="añadir 1"></a>';
-                echo '<a href="/subtract/' . $product->id . '" class="productos"><img src="/img/menos.png" alt="quitar 1"></a>';
-                echo '<a href="/remove/' . $product->id . '" class="productos"><img src="/img/papelera.png" alt="quitar todos"></a>';
-                echo '</span>';
-                echo '<span>Stock: ' . $product->stock . '</span>';
-                echo '</article>';
-            }
-            
-            unset($products);
-            unset($connection);
-            ?>
+         foreach ($products as $product) {
+            echo '<article class="producto">';
+            echo '<h2>' . $product->name . '</h2>';
+            echo '<span>(' . $product->category . ')</span>';
+            echo '<img src="/img/products/' . $product->image . '" alt="' . $product->name . '" class="imgProducto"><br>';
+            echo '<span>' . $product->price . ' €</span><br>';
+            echo '<span class="botonesCarrito">';
+            echo '<a href="/add/' . $product->id . '" class="productos"><img src="/img/mas.png" alt="añadir 1"></a>';
+            echo '<a href="/subtract/' . $product->id . '" class="productos"><img src="/img/menos.png" alt="quitar 1"></a>';
+            echo '<a href="/remove/' . $product->id . '" class="productos"><img src="/img/papelera.png" alt="quitar todos"></a>';
+            echo '</span>';
+            echo '<span>Stock: ' . $product->stock . '</span>';
+            echo '</article>';
+         }
+
+         unset($products);
+         unset($connection);
+         ?>
       </section>
-      <?php endif; ?>
-   </body>
+   <?php endif; ?>
+</body>
+
 </html>
